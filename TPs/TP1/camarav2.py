84684e7003e205204f546a305024bf8e8d383f67
JuanManuelPM <perezmenendez.jm@gmail.com>
2023-08-15 15:30:46 -0300

    borre boludeces

--- TPs/TP1/camarav2.py
+++ TPs/TP1/camarav2.py
@@ -12,17 +12,17 @@ def update_kernel_size(value):
     global kernel_size
     kernel_size = value
 
-
 cap = cv2.VideoCapture(0)  # 0 represents the default camera
 
 cv.namedWindow('Camera')
 
 cv.createTrackbar('Brightness', 'Camera', 0, 100, update_brightness)
 
-# Create a trackbar named 'Kernel Size' in the 'Camera' window
 cv2.createTrackbar('Kernel Size', 'Camera', 1, 20, update_kernel_size)
 
 brightness = 0  # Initial brightness value
+
+kernel_size = 1  # Initial kernel size
 
 while True:
     ret, frame = cap.read()  # Read a frame from the camera
@@ -33,6 +33,8 @@ while True:
 
     # Convert the frame to grayscale for denoising
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    ret2, thresh2 = cv.threshold(gray_frame, val, 255, cv.THRESH_BINARY_INV)
 
     # Create a kernel for morphological operation
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
