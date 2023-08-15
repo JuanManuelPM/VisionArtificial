0d1481915b7c1d87c1cf67aeff513cdc31052638
JuanManuelPM <perezmenendez.jm@gmail.com>
2023-08-15 19:11:42 -0300

    borre boludeces

--- TPs/TP1/camarav3.py
+++ TPs/TP1/camarav3.py
@@ -2,10 +2,20 @@ import cv2
 import numpy as np
 
 
-# Callback function for the trackbar
+def update_contour(value):
+    global contour_mode
+    contour_mode = value
+
+# Callback function for the kernel size trackbar
 def update_kernel_size(value):
     global kernel_size
     kernel_size = value
+
+
+# Callback function for the binary threshold trackbar
+def update_threshold(value):
+    global threshold_value
+    threshold_value = value
 
 
 # Initialize camera capture
@@ -14,13 +24,17 @@ cap = cv2.VideoCapture(0)  # 0 represents the default camera
 # Create a window for displaying the camera feed
 cv2.namedWindow('Camera')
 
-# Create a trackbar named 'Kernel Size' in the 'Camera' window
+# Create trackbars for kernel size and binary threshold
 cv2.createTrackbar('Kernel Size', 'Camera', 1, 20, update_kernel_size)
+cv2.createTrackbar('Threshold', 'Camera', 128, 255, update_threshold)
 
 kernel_size = 1  # Initial kernel size
+threshold_value = 128  # Initial threshold value
+contour_mode = 0
 
 while True:
     ret, frame = cap.read()  # Read a frame from the camera
+    frame = cv2.flip(frame, 1)
 
     if not ret:
         print("Error reading frame")
@@ -35,8 +49,14 @@ while True:
     # Apply opening operation to denoise the image
     denoised_frame = cv2.morphologyEx(gray_frame, cv2.MORPH_OPEN, kernel)
 
-    # Display the original and denoised frames
-    cv2.imshow('Camera', np.hstack((gray_frame, denoised_frame)))
+    # Apply binary thresholding to the denoised frame
+    _, binary_frame = cv2.threshold(denoised_frame, threshold_value, 255, cv2.THRESH_BINARY)
+
+    # Combine the denoised and binary thresholded frames side by side
+    combined_frame = np.hstack((frame, binary_frame))
+
+    # Display the combined frame
+    cv2.imshow('Camera', combined_frame)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):  # Press 'q' to exit
