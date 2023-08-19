9ee0a51e7c9bbbb7168c790a2c326bf448096cf1
Maxi Maglio <maximiliano.maglio@ing.austral.edu.ar>
2023-08-19 11:01:01 -0300

    ......

--- TPs/TP1/camarav3.py
+++ TPs/TP1/camarav3.py
@@ -28,7 +28,7 @@ cv2.namedWindow('Camera')
 # Create trackbars for kernel size and binary threshold
 cv2.createTrackbar('Kernel Size', 'Camera', 1, 20, update_kernel_size)
 cv2.createTrackbar('Threshold', 'Camera', 128, 255, update_threshold)
-cv2.createTrackbar('Contour', 'Camera', 0, 1, update_contour)
+cv2.createTrackbar('Contour', 'Camera', 100, 500, update_contour)
 
 kernel_size = 1  # Initial kernel size
 threshold_value = 128  # Initial threshold value
@@ -55,12 +55,15 @@ while True:
     _, binary_frame = cv2.threshold(denoised_frame, threshold_value, 255, cv2.THRESH_BINARY)
 
     # Apply contour detection if enabled
-    if contour_mode == 1:
-        contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+    # Find contours in the binary thresholded frame
+    contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Combine the original, denoised, binary thresholded, and contour frames horizontally
-        combined_frame = np.hstack((frame, denoised_frame, binary_frame))
+    # Draw contours on the original frame
+    contour_image = frame.copy()
+    cv2.drawContours(contour_image, contours, -1, (0, 255, 0), contour_threshold)
+
+    # Combine the modified original frame, denoised frame, and contour image
+    combined_frame = np.vstack((frame, denoised_frame, contour_image))
 
     # Display the combined frame
     cv2.imshow('Camera', combined_frame)
