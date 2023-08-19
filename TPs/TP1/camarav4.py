6552f4b17e23a381f2aba1b5c402f55b3c5f592b
MaxiMaglio <maximiliano.maglio@ing.austral.edu.ar>
2023-08-19 11:48:43 -0300

    contours

--- TPs/TP1/camarav4.py
+++ TPs/TP1/camarav4.py
@@ -16,18 +16,26 @@ def update_threshold(value):
     threshold_value = value
 
 
+# Callback function for the contour detection trackbar
+def update_contour_detection(value):
+    global contour_detection
+    contour_detection = value
+
+
 # Initialize camera capture
 cap = cv2.VideoCapture(0)  # 0 represents the default camera
 
 # Create a window for displaying the camera feed
 cv2.namedWindow('Camera')
 
-# Create trackbars for kernel size and binary threshold
+# Create trackbars for kernel size, binary threshold, and contour detection
 cv2.createTrackbar('Kernel Size', 'Camera', 1, 20, update_kernel_size)
 cv2.createTrackbar('Threshold', 'Camera', 128, 255, update_threshold)
+cv2.createTrackbar('Contour Detection', 'Camera', 0, 1, update_contour_detection)
 
 kernel_size = 1  # Initial kernel size
 threshold_value = 128  # Initial threshold value
+contour_detection = 0  # Initial contour detection value
 
 while True:
     ret, frame = cap.read()  # Read a frame from the camera
@@ -42,14 +50,21 @@ while True:
     # Apply opening operation to denoise the image
     denoised_frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
 
-    # Apply binary thresholding to the grayscale denoised frame
+    # Convert the denoised frame to grayscale for binary thresholding
     gray_denoised_frame = cv2.cvtColor(denoised_frame, cv2.COLOR_BGR2GRAY)
+
+    # Apply binary thresholding to the grayscale denoised frame
     _, binary_frame = cv2.threshold(gray_denoised_frame, threshold_value, 255, cv2.THRESH_BINARY)
 
     # Convert the binary frame to a 3-channel (color) image
     colored_binary_frame = cv2.cvtColor(binary_frame, cv2.COLOR_GRAY2BGR)
 
-    # Combine the original, denoised, and binary thresholded frames horizontally
+    # Find and draw contours if contour_detection is enabled
+    if contour_detection == 1:
+        contours, _ = cv2.findContours(binary_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
+
+    # Combine the original, denoised, binary thresholded, and contour frames horizontally
     combined_frame = np.hstack((frame, denoised_frame, colored_binary_frame))
 
     # Display the combined frame
